3031290878c22aa1caff0300b1ca46f8b05f84a8
aKirpichenok <a.kirpichenok99@gmail.com>
2022-06-09 17:57:31 +0300

    trainee

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LongPulling from "./LongPulling";
+import './App.css'
+import EventSourcing from "./EventSourcing";
+import WebSock from "./WebSock";
+
+const App = () => {
+  return (
+    <div>
+      <WebSock />
+    </div>
+  )
+}
+
+export default App
